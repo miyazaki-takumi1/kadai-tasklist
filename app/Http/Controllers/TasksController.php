99fca71c16591f600c18f4e68d6a37ecd665a388
takumi.miyazaki <Miyazaki.Takumi@trans-cosmos.co.jp>
2025-06-12 10:16:54 +0900

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -39,6 +39,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
         //メッセージを作成
         $task = new Task;
         $task->content = $request->content;
@@ -82,6 +86,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        //バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+
         //idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
 
