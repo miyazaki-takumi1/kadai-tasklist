51dff59a451656d79e679dcf889430be803f38ad
takumi.miyazaki <Miyazaki.Takumi@trans-cosmos.co.jp>
2025-06-16 16:07:57 +0900

    top page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<!-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -15,3 +15,16 @@
         </div>
     </div>
 </x-app-layout>
+@extends('layouts.app') -->
+
+@section('content')
+    <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
+        <div class="hero-content text-center my-10">
+            <div class="max-w-md mb-10">
+                <h2>Welcome to the Tasks</h2>
+                {{-- ユーザー登録ページへのリンク --}}
+                <a class="btn btn-primary btn-lg normal-case" href="{{ route('register') }}">Sign up now!</a>
+            </div>
+        </div>
+    </div>
+@endsection
